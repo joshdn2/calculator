ef0c49dec027f0a63450ea41b91888737df10c5d
Josh Danneman <joshdn2@gmail.com>
2025-02-18 18:23:05 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
 })
 export class AppComponent {
-  title = 'calculator';
+  currentNumber: string = '0';
+  firstOperand: number | null = null;
+  operator: string | null = null;
+  waitForSecondNumber: boolean = false;
+
+  handleNumber(value: string) {
+    if (this.waitForSecondNumber) {
+      this.currentNumber = value;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === '0' ? this.currentNumber = value : this.currentNumber += value;
+    }
+  } 
+
+  handleOperator(op: string) {
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.currentNumber);
+    } else if (this.operator) {
+      const result = this.calculate();
+      this.currentNumber = String(result);
+      this.firstOperand = result ?? null;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+  }
+
+  calculate() {
+    if (this.firstOperand === null || this.operator === null) return;
+    
+    const secondOperand = Number(this.currentNumber);
+    let result: number;
+
+    switch (this.operator) {
+      case '+':
+        result = this.firstOperand + secondOperand;
+        break;
+      case '-':
+        result = this.firstOperand - secondOperand;
+        break;
+      case '*':
+        result = this.firstOperand * secondOperand;
+        break;
+      case '/':
+        result = this.firstOperand / secondOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentNumber = String(result);
+    this.firstOperand = null;
+    this.operator = null;
+    return result;
+  }
+
+  clear() {
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
 }
